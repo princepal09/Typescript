31d418759c94e7b93aa9c49192bf2e94ebf5795a
Prince Pal <princepal262501@gmail.com>
2026-03-27 23:12:17 +0530

    added index.ts

--- 01_basics/index.ts
+++ 01_basics/index.ts
@@ -1,4 +1,15 @@
-console.log("I am Prince")
+// Todo1 . Write a program to add two numbers
+// Todo2 . how to catch errors and solve it?
+// Todo3 . TS configuration file
 
-let num = 5
-num = "prince"
+// * func define
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+const mul = (a: number, b: number): number => {
+  return a * b;
+};
+// * fun call
+console.log(sum(5, 12));
+console.log(mul(4, 5));
