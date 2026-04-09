c57f78460bba004ec5f3de1504de09d71b27437b
Prince Pal <princepal262501@gmail.com>
2026-04-09 22:11:54 +0530

    learnt the basic data type of typescript

---
+++ 01_basics/number.ts
@@ -0,0 +1,23 @@
+// var var_name:data_type = value
+
+var num1: number = 30;
+var num2: number = 40;
+
+var total: number = num1 + num2;
+
+// var num3 = "30"
+// var total : number = num1 + num3
+
+console.log(total);
+
+
+
+// PREFIX FOR OCTAL, HEXA, BINARY
+
+ var octal : number = 0o10001; // 0o PRefix
+ var hexa : number = 0x0001; // 0x Prefix
+ var binary : number = 0b00001 // 0b Prefix 
+
+ var data : number | string = 30
+ data = "Prince"
+
---
+++ 01_basics/string_boolean.ts
@@ -0,0 +1,18 @@
+var str : string = "Hello how are you "
+var str2 : string = 'Prince Pal'
+var str4 : string = `Monika`
+
+
+
+var age : number = 30
+var userName : string  = "Prince Pal"
+
+var info : string = `My name is ${userName} and my age is ${age} years old`
+console.log(info)
+
+var num : number = 100;
+// var data1 : string = num.toString()
+var data1 : string = "" + num;
+
+var item : boolean = true;
+var item2 : boolean = false
