10d2bf3bd8dc02f3c68596aa29dbceaa02543fac
Prince Pal <princepal262501@gmail.com>
2026-04-09 22:18:35 +0530

    learnt the null, undefined data type of typesccript

---
+++ 01_basics/null_undefined.js
@@ -0,0 +1,9 @@
+"use strict";
+var a = null;
+var b = undefined;
+var c = null;
+var userName2 = null;
+var login = true;
+if (true) {
+    userName2 = "ORPince pal ";
+}
---
+++ 01_basics/null_undefined.ts
@@ -0,0 +1,15 @@
+var a : null = null;
+
+var b: undefined = undefined;
+
+
+var c : null | string = null;
+
+var userName2 : null | string = null;
+
+var login = true
+
+if(true){
+    userName2 = "ORPince pal "
+}
+
