ff7560534b9d09c934fc7c96523bffd4c040646a
Prince Pal <princepal262501@gmail.com>
2026-04-14 15:54:19 +0530

    learnt generics

---
+++ 04_basics/generics.ts
@@ -0,0 +1,14 @@
+function fruits <T>(name : T) {
+    return name
+} 
+
+fruits("apple")
+fruits(10)
+fruits(false)
+
+function user19<T>(data : T[]){
+    return data
+
+}
+
+let user = user19([10,12,20])
